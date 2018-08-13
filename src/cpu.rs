6b1d5c220575b51756b36ae5a0e38422207e72f2
Austin Lasher <austinlasher@gmail.com>
2018-08-13 00:53:50 -0400

    Added JR and RET instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -82,7 +82,9 @@ impl Registers {
 pub struct CPU {
     regs: Registers,
     mem: Memory,
-    ir_enabled: bool
+    ir_enabled: bool,
+    quit: bool,
+    jumped: bool
 }
 
 impl CPU {
@@ -90,20 +92,56 @@ impl CPU {
         CPU {
             regs: Registers::new(),
             mem: mem,
-            ir_enabled: true
+            ir_enabled: true,
+            quit: false,
+            jumped: false,
         }
+    }
+
+    pub fn get_pc(&self) -> u16 {
+        self.regs.get_pc()
     }
 
     fn parse_u16(&self, iaddr: u16) -> u16 {
         ((self.mem.get(iaddr+1) as u16) << 8) | self.mem.get(iaddr) as u16
     }
 
+    // Utility function to split a two byte dword into a two-byte pair. Little end is first.
+    // Ex: 0xFF11 -> (0x11, 0xFF)
+    fn split_u16(dword: u16) -> (u8, u8) {
+        ((dword & 0xFF) as u8, (dword >> 8) as u8)
+    }
+
     // Push next_addr to stack, and jump to the jump_addr
     fn call(&mut self, jump_addr: u16, next_addr: u16) {
         self.regs.sp -= 2;
-        self.mem.set((next_addr & 0xFF) as u8, self.regs.sp);
-        self.mem.set((next_addr >> 8) as u8, self.regs.sp+1);
+        let split_addr = CPU::split_u16(next_addr);
+        self.mem.set(split_addr.0, self.regs.sp);
+        self.mem.set(split_addr.1, self.regs.sp+1);
         self.regs.pc = jump_addr;
+        self.jumped = true;
+    }
+
+    // Pop the topmost address from the stack, and jump to it.
+    fn ret(&mut self) {
+        let addr = self.parse_u16(self.regs.sp);
+        self.regs.sp += 2;
+        self.regs.pc = addr;
+        self.jumped = true;
+    }
+
+    // Jump relative to current PC, where offset is twos-complement 8-bit signed int.
+    fn jump_relative(&mut self, offset: u8) {
+        let addr = self.regs.pc as i32;
+        let addr = addr + (offset as i8) as i32;
+        if addr < 0 || addr > 0xFFFF {
+            println!("Fatal error: jumped out-of-bounds!");
+            self.quit = true;
+            return;
+        }
+
+        self.regs.pc = addr as u16;
+        self.jumped = true;
     }
 
     fn print_instruction_debug(&self, opname: &str, ncycles: u8, old_pc: u16, opsize: u8) {
@@ -119,10 +157,10 @@ impl CPU {
 
     // Run the instruction at the current PC, return true if successful.
     pub fn process(&mut self) -> bool {
-        let mut quit: bool = false;
-        let mut jump: bool = false;
+        if self.quit { return false; }
         let mut opname: String = String::from("UNDEFINED");
         let old_pc = self.regs.pc;
+        self.jumped = false;
 
         // TODO: Move these to a separate lookup buffer? It would clean up the
         //       giant match statement below a little bit.
@@ -152,73 +190,79 @@ impl CPU {
             0x01 => {
                 opname = String::from("LD BC,d16");
                 self.regs.set_bc(operand16);
-                cycles += 12;
+                cycles = 12;
                 opsize = 3;
             },
             0x06 => {
                 opname = String::from("LD B,d8");
                 self.regs.set_b(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x0E => {
                 opname = String::from("LD C,d8");
                 self.regs.set_c(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x11 => {
                 opname = String::from("LD DE,d16");
                 self.regs.set_de(operand16);
-                cycles += 12;
+                cycles = 12;
                 opsize = 3;
             },
             0x16 => {
                 opname = String::from("LD D,d8");
                 self.regs.set_d(operand8);
-                cycles += 8;
+                cycles = 8;
+                opsize = 2;
+            },
+            0x18 => {
+                opname = String::from("JR r8");
+                self.jump_relative(operand8);
+                cycles = 12;
                 opsize = 2;
             },
             0x1E => {
                 opname = String::from("LD E,d8");
                 self.regs.set_e(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x21 => {
                 opname = String::from("LD HL,d16");
                 self.regs.set_hl(operand16);
-                cycles += 12;
+                cycles = 12;
                 opsize = 3;
             },
             0x26 => {
                 opname = String::from("LD H,d8");
                 self.regs.set_h(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x2E => {
                 opname = String::from("LD L,d8");
                 self.regs.set_l(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x31 => {
                 opname = String::from("LD SP,d16");
                 self.regs.set_sp(operand16);
-                cycles += 12;
+                cycles = 12;
                 opsize = 3;
             },
             0x36 => {
                 opname = String::from("LD (hl),d8");
                 self.mem.set(operand8, self.regs.get_hl());
-                cycles += 12;
+                cycles = 12;
                 opsize = 2;
             },
             0x3E => {
                 opname = String::from("LD A,d8");
                 self.regs.set_a(operand8);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0x40 => {
@@ -255,7 +299,7 @@ impl CPU {
                 opname = String::from("LD B,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_b(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x47 => {
                 opname = String::from("LD B,A");
@@ -296,7 +340,7 @@ impl CPU {
                 opname = String::from("LD C,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_c(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x4f => {
                 opname = String::from("LD C,A");
@@ -337,7 +381,7 @@ impl CPU {
                 opname = String::from("LD D,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_d(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x57 => {
                 opname = String::from("LD D,A");
@@ -378,7 +422,7 @@ impl CPU {
                 opname = String::from("LD E,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_e(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x5f => {
                 opname = String::from("LD E,A");
@@ -419,7 +463,7 @@ impl CPU {
                 opname = String::from("LD H,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_h(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x67 => {
                 opname = String::from("LD H,A");
@@ -460,7 +504,7 @@ impl CPU {
                 opname = String::from("LD L,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_l(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x6f => {
                 opname = String::from("LD L,A");
@@ -471,48 +515,48 @@ impl CPU {
                 opname = String::from("LD (HL),B");
                 let r = self.regs.get_b();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x71 => {
                 opname = String::from("LD (HL),C");
                 let r = self.regs.get_c();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x72 => {
                 opname = String::from("LD (HL),D");
                 let r = self.regs.get_d();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x73 => {
                 opname = String::from("LD (HL),E");
                 let r = self.regs.get_e();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x74 => {
                 opname = String::from("LD (HL),H");
                 let r = self.regs.get_h();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x75 => {
                 opname = String::from("LD (HL),L");
                 let r = self.regs.get_l();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x76 => {
                 println!("Encountered HALT instruction, exiting!");
                 opname = String::from("HALT");
-                quit = true;
+                self.quit = true;
             },
             0x77 => {
                 opname = String::from("LD (HL),A");
                 let r = self.regs.get_a();
                 self.mem.set(r, self.regs.get_hl());
-                cycles += 8;
+                cycles = 8;
             },
             0x78 => {
                 opname = String::from("LD A,B");
@@ -548,7 +592,7 @@ impl CPU {
                 opname = String::from("LD A,(HL)");
                 let r = self.mem.get(self.regs.get_hl());
                 self.regs.set_a(r);
-                cycles += 8;
+                cycles = 8;
             },
             0x7f => {
                 opname = String::from("LD A,A");
@@ -558,54 +602,64 @@ impl CPU {
             0xC3 => {
                 opname = String::from("JP a16");
                 self.regs.pc = operand16;
-                jump = true;
-                cycles += 16;
+                self.jumped = true;
+                cycles = 16;
                 opsize = 3;
             },
             0xCB => {
                 // This should never happen, we should always append the prefix after CB, ex: 0xCB01
                 println!("Fatal error: encountered unadjusted 0xCB literal!");
                 opname = String::from("PREFIX CB");
-                quit = true;
+                self.quit = true;
             },
             0xCD => {
                 opname = String::from("CALL a16");
                 opsize = 3;
                 let next_addr = self.regs.pc + opsize as u16;
                 self.call(operand16, next_addr);
-                jump = true;
-                cycles += 24;
+                cycles = 24;
+            },
+            0xC9 => {
+                opname = String::from("RET");
+                self.ret();
+                cycles = 16;
+            },
+            0xD9 => {
+                opname = String::from("RETI");
+                self.ret();
+                self.ir_enabled = true;
+                cycles = 16;
             },
             0xE0 => {
                 opname = String::from("LDH (a8),A");
                 self.mem.set(self.regs.get_a(), 0xFF00 + (operand8 as u16));
-                cycles += 12;
+                cycles = 12;
                 opsize = 2;
             },
             0xE2 => {
                 opname = String::from("LD (C),A");
                 let addr = 0xFF00 + self.regs.get_c() as u16;
                 self.mem.set(self.regs.get_a(), addr);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0xEA => {
                 opname = String::from("LD (a16),A");
                 self.mem.set(self.regs.get_a(), operand16);
-                cycles += 16;
+                cycles = 16;
                 opsize = 3;
             },
             0xF0 => {
                 opname = String::from("LDH A,(a8)");
                 self.regs.set_a(self.mem.get(0xFF00 + (operand8 as u16)));
-                cycles += 12;
+                cycles = 12;
                 opsize = 2;
             },
             0xF2 => {
                 opname = String::from("LD (C),A");
                 let addr = 0xFF00 + self.regs.get_c() as u16;
                 self.mem.set(self.regs.get_a(), addr);
-                cycles += 8;
+                cycles = 8;
                 opsize = 2;
             },
             0xF3 => {
@@ -615,7 +669,7 @@ impl CPU {
             0xFA => {
                 opname = String::from("LD A,(a16)");
                 self.regs.set_a(self.mem.get(operand16));
-                cycles += 16;
+                cycles = 16;
                 opsize = 3;
             },
             0xFB => {
@@ -625,18 +679,18 @@ impl CPU {
             _ => {
                 println!("Fatal error: undefined instruction!");
                 opname = format!("UNDEFINED 0x{:02X}", opcode);
-                quit = true;
+                self.quit = true;
             }
         }
 
         self.print_instruction_debug(opname.as_str(), cycles, old_pc, opsize);
 
         // Standard PC increment, a single instruction
-        if !jump {
+        if !self.jumped {
             self.regs.pc += opsize as u16;
         }
 
 
-        !quit
+        !self.quit
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,22 @@ fn main() {
     mem.load_rom_file(&fname);
 
     let mut z80 = cpu::CPU::new(mem);
+    let mut last_pc = z80.get_pc();
+    let mut loop_cnt = 0;
 
     // Now, run instructions *literally* forever!
     loop {
         if !z80.process() { break; }
+        if z80.get_pc() == last_pc {
+            loop_cnt += 1;
+        } else {
+            last_pc = z80.get_pc();
+        }
+        if loop_cnt >= 20 {
+            // Just a really simple loop detector, since apparently Ctrl+C isn't a thing in Rust?
+            println!("Fatal error: stuck in a single-instruction loop.");
+            break;
+        }
+
     }
 }
